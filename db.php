6197c6aceb5c461f7b77de0fc6569d723f9823ff
Voxellate <LukeDugdale@icloud.com>
2017-01-11 20:04:27 +1000

    Revert "Added basic cart mechanics"
    This reverts commit 6763af6a994659892c556c1a2c678ec2f8e2c86f.

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-function dbquery($query){
-    $db = mysqli_connect("localhost", "root", "password", "pizza-planet");
-    $result = mysqli_query($db, $query) or die(mysqli_error($db));
-    return $result;
-}
